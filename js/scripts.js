2769a8ec0e10ea4ebea2e119ec9f1975a6edbc6a
Alex Siegberg <alex.siegberg@gmail.com>
2019-06-14 10:48:46 -0700

    add initial logic to translate numbers to new words

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
+//back-end logic
+const outputMessages = ["Beep!", "Boop!", "I'm sorry, Dave. I'm afraid I can't do that."]
+
+function gatherInputData() {
+  return $("#number").val();
+};
+
+function beepBoop() {
+  var output;
+  var userInput = gatherInputData().split("");
+  userInput.forEach(function(number) {
+    if(number === 3) {
+      return output;
+    }
+  });
+};
+
+//front-end logic
 $(document).ready(function(){
+
   $("#boop-form").submit(function(event) {
     event.preventDefault();
-    var userInput = parseInt($("#number").val());
-    $(".output").append(userInput + " ");
+    beepBoop();
   });
+
 });
