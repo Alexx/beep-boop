4c93a898884646a922832104d796897190894b70
Alex Siegberg <alex.siegberg@gmail.com>
2019-06-14 10:36:38 -0700

    add jQuery to extract data from form

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-
+  $("#boop-form").submit(function(event) {
+    event.preventDefault();
+    var userInput = parseInt($("#number").val());
+    $(".output").append(userInput + " ");
+  });
 });
